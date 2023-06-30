1f9b356704e39c7d80fdef9929d7dfcb52aba35a
github-uehara <k.uehara32@dcom-net.jp>
2023-06-30 12:22:17 +0900

    2023/06/30

---
+++ CSharp/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(102, 15);
+            label1.TabIndex = 0;
+            label1.Text = "ラムダ式と匿名関数";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 27);
+            button1.Name = "button1";
+            button1.Size = new Size(385, 23);
+            button1.TabIndex = 1;
+            button1.Text = "ラムダ式を使用して匿名関数を作成する";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += Button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(403, 27);
+            button2.Name = "button2";
+            button2.Size = new Size(385, 23);
+            button2.TabIndex = 2;
+            button2.Text = "ステートメント形式のラムダ";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += Button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(12, 56);
+            button3.Name = "button3";
+            button3.Size = new Size(385, 23);
+            button3.TabIndex = 3;
+            button3.Text = "ラムダ式の入力パラメーター";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += Button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(403, 56);
+            button4.Name = "button4";
+            button4.Size = new Size(385, 23);
+            button4.TabIndex = 4;
+            button4.Text = "非同期ラムダ";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += Button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(12, 85);
+            button5.Name = "button5";
+            button5.Size = new Size(385, 23);
+            button5.TabIndex = 5;
+            button5.Text = "ラムダ式とタプル";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += Button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(403, 85);
+            button6.Name = "button6";
+            button6.Size = new Size(385, 23);
+            button6.TabIndex = 6;
+            button6.Text = "標準クエリ演算子を使用したラムダ";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += Button6_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+    }
+}
